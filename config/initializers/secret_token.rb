359ec3ccb6d4a1df3565f0f443456d34b6e34156
Casey Rosengren <nocashvaluedrumz@gmail.com>
2013-10-29 17:16:23 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twiki::Application.config.secret_token = '4a47d801099cab26ffad1c5d465b08b455cc60e8c47ced2965f693b1915591768a0d6b03a11c71e3016f180d9f5491cf555196d27f12b112b7910023bdd37320'
